52913cf44de39b5d8fd5f66e043626ece03f7f6d
Peyton Gilliam <bryant.peyton@yahoo.com>
2022-01-17 18:03:14 -0600

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,42 @@
+print("Hello World")
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("EL Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+if "Arapahoe" in counties and "El Paso" not in counties:
+    print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+for county in counties:
+    print(county)
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+for i in range(len(counties)):
+    print(counties[i])
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
